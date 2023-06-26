857daa17d29647ee91196154e26bb491b0c58e94
Davgcore <102384610+Davgcore@users.noreply.github.com>
2023-06-26 18:38:35 -0500

    solving error from imports

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,29 +1,38 @@
 import { useForm } from "react-hook-form"
-import useFetch from "../hooks/useFetch"
 import useAuth from "../hooks/useAuth"
-
 
 const LoginPage = () => {
 
-  const { register, reset, handleSubmit } = useForm()
-  const { loginUser } = useAuth()
-    
-  const submit = data => {
-    const url = `https://e-commerce-api-v2.academlo.tech/api/v1/users/login`
-    loginUser(url, data)
-  }
+    const {register, reset, handleSubmit} = useForm()
+
+    const {loginUser} = useAuth()
+
+
+    const submit = data => {
+        const url = 'https://e-commerce-api-v2.academlo.tech/api/v1/users/login'
+        loginUser(url,data)
+        reset(
+            {
+              email: '',
+              password:''
+            }
+          )
+    }
+
 
   return (
     <div>
       <form onSubmit={handleSubmit(submit)}>
         <div>
           <label htmlFor="email">Email</label>
-          <input {...register('email')} type="email" id="email" />
+          <input {...register('email')}type="text" id="email" />
         </div>
+
         <div>
           <label htmlFor="password">Password</label>
-          <input {...register('password')} type="password" id="password" />
+          <input {...register('password')}type="password" id="password" />
         </div>
+
         <button>Login</button>
       </form>
     </div>
--- src/pages/ProducIdPage.jsx
+++ src/pages/ProducIdPage.jsx
@@ -21,7 +21,7 @@ const ProducIdPage = () => {
         <ProductInfo 
         product={product}
         />
-
+        
         <SimilarProducts />
     </div>
     )
