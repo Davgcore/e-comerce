82837caf48257f2cf3b5c0492aac1c49616c13a0
Laura Marcela Orjuela <132188055+Lmorjuela@users.noreply.github.com>
2023-06-27 17:36:26 -0500

    arreglé bug de register

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,47 +1,56 @@
 import { useForm } from "react-hook-form"
 import useAuth from "../hooks/useAuth"
 
+
 const RegisterPage = () => {
 
-  const { register, handleSubmit, reset } = useForm()
-  const { createNewUser } = useAuth()
+    const  {register, handleSubmit, reset } = useForm()
+    
+    const {createUser} = useAuth()
 
 
-  const submit = data => {
-    const url = 'https://e-commerce-api-v2.academlo.tech/api/v1/users'
-    createNewUser(url, data)
-    reset ({
-      firstName: '', 
-      lastName: '',
-      email: '',
-      password: '',
-      phone: ''
-    })
-  }
-
-  return (
+    const submit = data =>{
+      const url = 'https://e-commerce-api-v2.academlo.tech/api/v1/users'
+      createUser(url , data)
+      reset(
+        {
+          firstName: '',
+          lastName: '',
+          email: '',
+          phone:'',
+          password:'',
+        }
+      )
+    }
+  
+    return (
     <div>
       <form onSubmit={handleSubmit(submit)}>
         <div>
           <label htmlFor="firstName">First name</label>
-          <input { ...register('firstName')} type="text" id="firstName" />
+          <input {...register('firstName')}type="text" id="firstName" />
         </div>
+
         <div>
           <label htmlFor="lastName">Last name</label>
-          <input { ...register('lastName')} type="text" id="lastName" />
+          <input {...register('lastName')}type="text" id="lastName" />
         </div>
+
         <div>
           <label htmlFor="email">Email</label>
-          <input { ...register('email')} type="email" id="email" />
+          <input {...register('email')}type="email" id="email" />
         </div>
+
         <div>
           <label htmlFor="password">Password</label>
-          <input { ...register('password')} type="password" id="password" />
+          <input {...register('password')}type="password" id="password" />
         </div>
+
         <div>
           <label htmlFor="phone">Phone</label>
-          <input { ...register('phone')} type="number" id="phone" />
+          <input {...register('phone')}type="number" id="phone" />
         </div>
+
         <button>Submit</button>
       </form>
     </div>
